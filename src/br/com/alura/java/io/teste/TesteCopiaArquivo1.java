1d1a22b7d842750d9059d33d6ee6a8287b747793
Alice <am171696@alunos.unisanta.br>
2020-10-25 09:39:57 -0300

    Renomeanndo classe TesteCopiaArquivo

--- src/br/com/alura/java/io/teste/TesteCopiaArquivo1.java
+++ src/br/com/alura/java/io/teste/TesteCopiaArquivo1.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class TesteCopiaArquivo {
+public class TesteCopiaArquivo1 {
 
 	public static void main(String[] args) throws IOException {
 		int conteudo; 		
