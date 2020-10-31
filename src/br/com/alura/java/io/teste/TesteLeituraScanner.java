ed1c4046019d8186bba96335482fae765e616b03
Alice <am171696@alunos.unisanta.br>
2020-10-30 21:33:03 -0300

    Leitura de um arquivo por meio do Scanner

---
+++ src/br/com/alura/java/io/teste/TesteLeituraScanner.java
@@ -0,0 +1,36 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeituraScanner {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		
+		while(scanner.hasNext()) {
+			String linha = scanner.nextLine();		
+			System.out.println(linha);
+			
+			Scanner scannerLinha = new Scanner(linha);
+			scannerLinha.useDelimiter(",");
+			scannerLinha.useLocale(Locale.US);
+			
+			String conta = scannerLinha.next();
+			int numero = scannerLinha.nextInt();
+			int agencia = scannerLinha.nextInt();
+			String titular = scannerLinha.next();
+			double saldo = scannerLinha.nextDouble();
+			
+			System.out.println("Tipo de conta: " + conta);
+			System.out.println("Número da conta: " + numero);
+			System.out.println("Agencia: " + agencia);
+			System.out.println("Titular: " + titular);
+			System.out.println("Saldo: R$" + saldo);
+			System.out.println("----------------------");
+		}
+	}
+
+}
